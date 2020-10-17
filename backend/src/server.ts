d30a8d41e82b4522b72e4595f6bcd08065cec3a9
joseadilson <joseadilsson@gmail.com>
2020-10-17 14:21:00 -0300

    finish web.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import './database/connection';
 
@@ -7,6 +8,7 @@ import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
---
+++ web/src/utils/mapIcon.ts
@@ -0,0 +1,13 @@
+import leaflet from 'leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+const mapIcon = leaflet.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [0, -60]
+})
+
+export default mapIcon;
